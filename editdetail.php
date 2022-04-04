670a2852bab61d75733d961ede2125a1f7615881
hailm <hai.lemanh.1310@gmail.com>
2022-04-04 14:26:55 +0700

    edit detail

---
+++ editdetail.php
@@ -0,0 +1,121 @@
+<?php $data["title"] = "Edit Detail"; ?>
+<?php $body = '' ?>
+<?php require "template-parts/layouts/header.php"; ?>
+    <div class="editing__section">
+        <div class="container">
+            <div class="editing__item30 text-center">
+                <div>えらんだ写真を表示しています。</div>
+                <div>※プレビュー画面ではありません</div>
+            </div>
+            <div class="editing__item30">
+                <ul class="MuiList-root jss201 MuiList-padding">
+                    <li class="MuiListItem-root jss203 MuiListItem-gutters"><img loading="lazy"
+                                                                                 class="jss204"
+                                                                                 src="https://storage.googleapis.com/toppan-mintsuku-stage.appspot.com/53NFBfBxWVz0tqCmt94l/049f8c4e-7c0d-4970-80f4-a6f914e3a5c5/desktop.jpeg"
+                                                                                 alt="desktop_image.jpeg">
+                    </li>
+                    <li class="MuiListItem-root jss203 MuiListItem-gutters"><img loading="lazy"
+                                                                                 class="jss204"
+                                                                                 src="https://storage.googleapis.com/toppan-mintsuku-stage.appspot.com/53NFBfBxWVz0tqCmt94l/1857e336-48b6-4d0c-88ff-c978f570efd9/desktop.jpeg"
+                                                                                 alt="desktop_image.jpeg">
+                    </li>
+                    <li class="MuiListItem-root jss203 MuiListItem-gutters"><img loading="lazy"
+                                                                                 class="jss204"
+                                                                                 src="https://storage.googleapis.com/toppan-mintsuku-stage.appspot.com/53NFBfBxWVz0tqCmt94l/1eec0f7c-8349-40bb-b417-36d54bb5f74b/desktop.jpeg"
+                                                                                 alt="desktop_image.jpeg">
+                    </li>
+                    <li class="MuiListItem-root jss203 MuiListItem-gutters"><img loading="lazy"
+                                                                                 class="jss204"
+                                                                                 src="https://storage.googleapis.com/toppan-mintsuku-stage.appspot.com/53NFBfBxWVz0tqCmt94l/2b01ce18-3b68-4f31-a7e2-cbe298b64563/desktop.jpeg"
+                                                                                 alt="desktop_image.jpeg">
+                    </li>
+                    <li class="MuiListItem-root jss203 MuiListItem-gutters"><img loading="lazy"
+                                                                                 class="jss204"
+                                                                                 src="https://storage.googleapis.com/toppan-mintsuku-stage.appspot.com/53NFBfBxWVz0tqCmt94l/2962dac8-eb5a-4298-a097-2ccd5e7a7886/desktop.jpeg"
+                                                                                 alt="desktop_image.jpeg">
+                    </li>
+                    <li class="MuiListItem-root jss203 MuiListItem-gutters"><img loading="lazy"
+                                                                                 class="jss204"
+                                                                                 src="https://storage.googleapis.com/toppan-mintsuku-stage.appspot.com/53NFBfBxWVz0tqCmt94l/34c9caea-d2dc-4580-9b14-496234e1c548/desktop.jpeg"
+                                                                                 alt="desktop_image.jpeg">
+                    </li>
+                    <li class="MuiListItem-root jss203 MuiListItem-gutters"><img loading="lazy"
+                                                                                 class="jss204"
+                                                                                 src="https://storage.googleapis.com/toppan-mintsuku-stage.appspot.com/53NFBfBxWVz0tqCmt94l/55dcd2c8-180d-4687-9264-dae298e4fb14/desktop.jpeg"
+                                                                                 alt="desktop_image.jpeg">
+                    </li>
+                    <li class="MuiListItem-root jss203 MuiListItem-gutters"><img loading="lazy"
+                                                                                 class="jss204"
+                                                                                 src="https://storage.googleapis.com/toppan-mintsuku-stage.appspot.com/53NFBfBxWVz0tqCmt94l/636c9971-08d6-441c-9a32-a928a84592ff/desktop.jpeg"
+                                                                                 alt="desktop_image.jpeg">
+                    </li>
+                    <li class="MuiListItem-root jss203 MuiListItem-gutters"><img loading="lazy"
+                                                                                 class="jss204"
+                                                                                 src="https://storage.googleapis.com/toppan-mintsuku-stage.appspot.com/53NFBfBxWVz0tqCmt94l/71a5e8f1-e170-4fc2-9e70-01421abb9def/desktop.jpeg"
+                                                                                 alt="desktop_image.jpeg">
+                    </li>
+                    <li class="MuiListItem-root jss203 MuiListItem-gutters"><img loading="lazy"
+                                                                                 class="jss204"
+                                                                                 src="https://storage.googleapis.com/toppan-mintsuku-stage.appspot.com/53NFBfBxWVz0tqCmt94l/6e014f06-8a88-47b6-a050-5b7df889450b/desktop.jpeg"
+                                                                                 alt="desktop_image.jpeg">
+                    </li>
+                    <li class="MuiListItem-root jss203 MuiListItem-gutters"><img loading="lazy"
+                                                                                 class="jss204"
+                                                                                 src="https://storage.googleapis.com/toppan-mintsuku-stage.appspot.com/53NFBfBxWVz0tqCmt94l/a340389f-eaef-4be8-a71d-3d87af8b2d34/desktop.jpeg"
+                                                                                 alt="desktop_image.jpeg">
+                    </li>
+                </ul>
+            </div>
+            <div class="editing__item30">
+                <div class="editing__item20 text-center">
+                    <div class="editing__title">Nguyễn Thị Hoài Vân</div>
+                </div>
+                <div class="editing__item20">
+                    <div class="editing__box1">
+                        <div>
+                            <div class="editing__title">￥6,600</div>
+                        </div>
+                        <div>
+                            <div class="editing__box1__img">A4 / 40 P / フルカラー / デモ</div>
+                            <div class="editing__box1__time">2022/04/03 20:41 <a href="" data-toggle="modal" data-target="#myModal">削除する</a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="editing__item30 text-center">
+                <button class="btn btn-default editing__btnAdd">新規作成</button>
+            </div>
+            <div class="editing__item30 text-center">
+                <a href="" class="btn btn-default editing__back">一覧に戻る</a>
+            </div>
+        </div>
+    </div>
+    <!-- Modal -->
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div class="modal-dialog editing__modal__dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <div class="text-center">
+                        <div class="editing__item20">
+                            <div class="editing__modal__txt1">フォトプロジェクトを削除します。よろしいですか？</div>
+                        </div>
+                        <div class="editing__item20">
+                            <div class="editing__modal__txt2">一度削除すると戻すことはできませんので、ご注意ください。</div>
+                        </div>
+                        <div class="editing__item20">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <a href="" class="btn btn-default editing__modal__btn">キャンセル</a>
+                                </div>
+                                <div class="col-md-6">
+                                    <a href="" class="btn btn-danger editing__modal__btn">削除する</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php require "template-parts/layouts/footer.php"; ?>
